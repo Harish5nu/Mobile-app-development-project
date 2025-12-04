1bc85d87753307e7ed18eed10f0e88f6604c55ab
Harish5nu <saudharish770@gmail.com>
2025-12-04 08:09:26 +0545

    changed in page

--- app/src/main/java/com/mobile/myapp/secondActivity.kt
+++ app/src/main/java/com/mobile/myapp/secondActivity.kt
@@ -1,23 +1,15 @@
 package com.mobile.myapp
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+class secondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
-        val btnRegister = findViewById<Button>(R.id.btnRegister)
-        btnRegister.setOnClickListener {
-
-        }
+        setContentView(R.layout.activity_second)
     }
 }
